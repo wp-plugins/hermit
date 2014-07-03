b0073eeb510c79051b69db15b916c60cab8257a1
afcold <afcold@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-03 02:26:14 +0000

    1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/hermit/trunk@942541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hermit.php
+++ hermit.php
@@ -3,12 +3,12 @@
 Plugin Name: Hermit
 Plugin URI: http://mufeng.me/hermit-for-wordpress.html
 Description: 虾米音乐播放器 Hermit for wordpress xiami music player
-Version: 1.3.0
+Version: 1.3.2
 Author: Mufeng
 Author URI: http://mufeng.me
 */
 
-define('VERSION', '1.3.0');
+define('VERSION', '1.3.2');
 define('HERMIT_PLUGIN_URL', dirname(__FILE__));
 
 global $HMT, $HMTJSON;
