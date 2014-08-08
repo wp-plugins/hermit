2328b91142418ee07ec99732a0f6fd42c814e009
afcold <afcold@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-08 03:57:32 +0000

    1.5.0
    git-svn-id: https://plugins.svn.wordpress.org/hermit/trunk@962168 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.json.php
+++ class.json.php
@@ -1,19 +1,24 @@
 <?php
 class HermitJson{
-	const API_URL_PREFIX = "http://www.xiami.com/app";
-	const SONG_URL = "/android/song/id/";
-	const ALBUM_URL = "/iphone/album/id/";
-	const COLLECT_URL = "/android/collect?id=";
-	const SONG_KEY_PREFIX = "/song/";
-	const ALBUM_KEY_PREFIX = "/album/";
-	const COLLECT_KEY_PREFIX = "/collect/";
+	const API_URL_PREFIX = "http://m.xiami.com/web/get-songs?type=0&rtype=";
+	const SONG_URL = "song&id=";
+	const ALBUM_URL = "album&id=";
+	const COLLECT_URL = "collect&id=";
+	const TOKEN_KEY = "&_xiamitoken=";
+	const SONG_KEY_PREFIX = "xiami/song/";
+	const ALBUM_KEY_PREFIX = "xiami/album/";
+	const COLLECT_KEY_PREFIX = "xiami/collect/";
+	const XIAMI_TOKEN_KEY = "_xiamitoken";
+
+	private $token;
 
 	public function __construct(){
+		$this->get_token();
 	}
 
 	public function song($song_id){
 		$key = self::SONG_KEY_PREFIX . $song_id;
-		$url = self::API_URL_PREFIX . self::SONG_URL . $song_id;
+		$url = self::API_URL_PREFIX . self::SONG_URL . $song_id . self::TOKEN_KEY . $this->token;
 
 		$cache = $this->get_cache($key);
 		if( $cache ) return $cache;
@@ -21,18 +26,12 @@ class HermitJson{
 		$response = $this->http($url);
 
 		if(  $response && $response['status'] == "ok" ){
-
-			if( $response["song"]["song_lrc"] ){
-				$song_lrc = $this->get_song_lrc($response["song"]["song_lrc"]);
-			}
-
 		    $result = array(
-			    "song_id" => $response["song"]["song_id"],
-			    "song_title" => $response["song"]["song_name"],
-				"song_author" => $response["song"]["artist_name"],
-				"song_cover" => $response["song"]["song_logo"],
-				"song_src" => $response["song"]["song_location"],
-				"song_lrc" => $song_lrc
+			    "song_id" => $response["data"][0]["id"],
+			    "song_title" => $response["data"][0]["title"],
+				"song_author" => $response["data"][0]["author"],
+				"song_cover" => $response["data"][0]["cover"],
+				"song_src" => $response["data"][0]["src"]
 			);
 
 		    $this->set_cache($key, $result);
@@ -63,45 +62,40 @@ class HermitJson{
 
 	public function album($album_id){
 		$key = self::ALBUM_KEY_PREFIX . $album_id;
-		$url = self::API_URL_PREFIX . self::ALBUM_URL . $album_id;
+		$url = self::API_URL_PREFIX . self::ALBUM_URL . $album_id . self::TOKEN_KEY . $this->token;
 
 		$cache = $this->get_cache($key);
 		if( $cache ) return $cache;
 
 		$response = $this->http($url); 
 
-		if(  $response["status"]=="ok" && $response["album"] ){
-			$result = $response["album"];
-			$count = count($result["songs"]);
+		if(  $response["status"]=="ok" && $response["data"] ){
+			$result = $response["data"];
+			$count = count($result);
 
 			if( $count < 1 ) return false;
 
 			$album = array(
-				"album_id" => $result["album_id"],
-				"album_title" => $result["title"],
-				"album_author" => '',
+				"album_id" => $album_id,
+				"album_title" => "",
+				"album_author" => "",
 				"album_type" => "albums",
-				"album_cover" => $result["album_logo"],
+				"album_cover" => "",
 				"album_count" => $count
 			);
 
-			foreach($result["songs"] as $key => $value){
-				$song_id = $value["song_id"];
-				
-				if( $value["lyric"] ){
-					$song_lrc = $this->get_song_lrc($value["lyric"]);
-				}
-				
+			foreach($result as $key => $value){
+
 				$album["songs"][] = array(
-					"song_id" => $song_id,
-					"song_title" => $value["name"],
-					"song_length" => $value["length"],
-					"song_src" => $value["location"],
-					"song_author" => $value["singers"],
-					"song_cover" => $result["album_logo"],
-					"song_lrc" => $song_lrc
+					"song_id" => $value["id"],
+					"song_title" => $value["title"],
+					"song_length" => "",
+					"song_src" => $value["src"],
+					"song_author" => $value["author"],
+					"song_cover" => $result["cover"]
 				);
-				$album["album_author"] = $value["singers"];
+				$album["album_author"] = $value["author"];
+				$album["album_cover"] = $value["cover"];
 			}
 
 			$this->set_cache($key, $album);
@@ -113,44 +107,41 @@ class HermitJson{
 
 	public function collect($collect_id){
 		$key = self::COLLECT_KEY_PREFIX . $collect_id;
-		$url = self::API_URL_PREFIX . self::COLLECT_URL . $collect_id;
+		$url = self::API_URL_PREFIX . self::COLLECT_URL . $collect_id . self::TOKEN_KEY . $this->token;
 
 		$cache = $this->get_cache($key);
 		if( $cache ) return $cache;
 
 		$response = $this->http($url); 
 
-		if(  $response["status"]=="ok" && $response["collect"] ){
-			$result = $response["collect"];
-			$count = count($result["songs"]);
+		if(  $response["status"]=="ok" && $response["data"] ){
+			$result = $response["data"];
+			$count = count($result);
 
 			if(  $count < 1 ) return false;
 
 			$collect = array(
-				"collect_id" => $result["id"],
-				"collect_title" => $result["name"],
-				"collect_author" => $result["nick_name"],
+				"collect_id" => $rcollect_id,
+				"collect_title" => '',
+				"collect_author" => '',
 				"collect_type" => "collects",
-				"collect_cover" => $result["logo"],
+				"collect_cover" => '',
 				"collect_count" => $count
 			);
 
-			foreach($result["songs"] as $key => $value){
-				$song_id = $value["song_id"];
-				
-				if( $value["lyric"] ){
-					$song_lrc = $this->get_song_lrc($value["lyric"]);
-				}
-				
+			foreach($result as $key => $value){
+
 				$collect["songs"][] = array(
-					"song_id" => $song_id,
-					"song_title" => $value["name"],
+					"song_id" => $value["id"],
+					"song_title" => $value["title"],
 					"song_length" => 0,
-					"song_src" => $value["location"],
-					"song_author" => $value["singers"],
-					"song_cover" => $result["logo"],
-					"song_lrc" => $song_lrc
+					"song_src" => $value["src"],
+					"song_author" => $value["author"],
+					"song_cover" => $result["cover"]
 				);
+
+				$collect["collect_author"] = $value["author"];
+				$collect["collect_cover"] = $value["cover"];
 			}
 			$this->set_cache($key, $collect);
 			return $collect;
@@ -163,22 +154,19 @@ class HermitJson{
 		if( !$url ){
 			return false;
 		}
-		
-		$ip = "42.156.140.238";
 
         $header = array(
-            'Host: www.xiami.com',
-            'User-Agent: Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19',
-            'X-FORWARDED-FOR:'.$ip,
-			'CLIENT-IP:'.$ip,
-			'Proxy-Connection:keep-alive',
-			'X-Requested-With:XMLHttpRequest'
+            'Host: m.xiami.com',
+            'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53',
+            'Cookie: _xiamitoken='.$this->token.'; visit=1',
+            'Proxy-Connection:keep-alive',
+            'X-Requested-With:XMLHttpRequest'
         );
 
 		$ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_REFERER, 'http://www.xiami.com/web/spark');
+		curl_setopt($ch, CURLOPT_REFERER, 'http://m.xiami.com/');
         $cexecute = curl_exec($ch);
 		@curl_close($ch);
 
@@ -190,39 +178,31 @@ class HermitJson{
 		}
 	}
 
-	public function get_song_lrc($lrc_url){
+	private function get_token(){
+		$token = get_transient(self::XIAMI_TOKEN_KEY);
 
-		$content = @file_get_contents($lrc_url);
-		$cache = $this->parse_lrc($content);
+		if( $token ){
+			$this->token = $token;
+		}else{
+			$XM_head = wp_remote_head('http://m.xiami.com', array(
+				'headers' => array(
+				    'Host: m.xiami.com',
+				    'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D257 Safari/9537.53',
+				    'Proxy-Connection:keep-alive',
+				    'X-Requested-With:XMLHttpRequest'
+				)
+			));
 
-		return $cache;
-	}
+			$cookies = $XM_head['cookies'];
 
-	private function parse_lrc($lrc_content){
-		$now_lrc = array();
-		$lrc_row = explode("\n", $lrc_content);
+			foreach ($cookies as $key => $cookie) {
+				if( $cookie->name == '_xiamitoken' ){
+					$this->token = $cookie->value;
 
-		foreach ($lrc_row as $key => $value) {
-			$tmp = explode("]", $value);
-
-			foreach ($tmp as $key => $val) {
-				$tmp2 = substr($val, 1, 8);
-				$tmp2 = explode(":", $tmp2);
-
-				$lrc_sec = intval( $tmp2[0]*60 + $tmp2[1]*1 );
-
-				if( is_numeric($lrc_sec) && $lrc_sec > 0){
-					$count = count($tmp);
-					$lrc = trim($tmp[$count-1]);
-
-					if( $lrc != "" ){
-						$now_lrc[$lrc_sec] = $lrc;  
-					}
+					set_transient(self::XIAMI_TOKEN_KEY, $this->token, 60*60*100);
 				}
-			}
+			}			
 		}
-
-		return $now_lrc;	
 	}
 
 	public function get_cache($key){
--- hermit.php
+++ hermit.php
@@ -3,12 +3,12 @@
 Plugin Name: Hermit
 Plugin URI: http://mufeng.me/hermit-for-wordpress.html
 Description: 虾米音乐播放器 Hermit for wordpress xiami music player
-Version: 1.4.3
+Version: 1.5.0
 Author: Mufeng
 Author URI: http://mufeng.me
 */
 
-define('VERSION', '1.4.3');
+define('VERSION', '1.5.0');
 define('HERMIT_PLUGIN_URL', dirname(__FILE__));
 
 global $HMT, $HMTJSON;
